29e93adc4a6be516c2643f20c2d549bad61ae1b5
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-02 22:18:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,11 +8,12 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   new_value = [ 
-    { :name =>  "Don Gately", :occupation => "Live-in Staff" },
-    { :name => "Joelle van Dyne", :occupation => "Radio Personality" },
-    { :name => "Pat Monteseian", :occupation => "Staff" },
-    { :name =>  "Kate Gompert", :occupation => "None" },
-    { :name =>  "Bruce Green", :occupation => "Fan of Mildred" } ]
+    Don_G,
+    Joelle_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+    ]
     
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
